76ee111696ad2a891cd3947be68c91dd37a0ec45
dowhep <caogangshen@gmail.com>
2020-02-15 10:59:04 -0800

    secretify

--- Herokuuu/settings.py
+++ Herokuuu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zc6c08!o44o)ta-^_y(d(=zp9#0ncab8nj67482ce1lj2vu0*4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
