ed25dc87a8821a28797885eb6b0b98e8f50f8929
dowhep <caogangshen@gmail.com>
2020-02-15 10:56:13 -0800

    Restart

--- app/views.py
+++ app/views.py
@@ -1,45 +1,5 @@
-"""
-Definition of views.
-"""
+from django.http import HttpResponse
 
-from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpRequest
-
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
-
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
-
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+def index(request):
+    output = "hi" 
+    return HttpResponse(output)
