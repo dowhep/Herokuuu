c5f0eb7421262ee497d2a2d54211bc95dd284e90
dowhep <caogangshen@gmail.com>
2020-02-15 11:09:46 -0800

    Here

--- Herokuuu/settings.py
+++ Herokuuu/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'zc6c08!o44o)ta-^_y(d(=zp9#0ncab8nj67482ce1lj2vu0*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
